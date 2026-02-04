5eb0ebb212aaa70aabb9c280fd8da17c5e14bc1a
will wade <willwade@gmail.com>
2026-02-04 11:38:57 +0000

    Update vite.config.js

--- apps/partner-web/vite.config.js
+++ apps/partner-web/vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/partner/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
